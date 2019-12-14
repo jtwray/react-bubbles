e97504cda4343252bac67040ce93c8dc7887876d
tucker <tuckerwray84@gmail.com>
2019-12-14 00:57:12 -0500

    adds a new stinking color yeeeeeet

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -11,32 +11,39 @@ const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
-  const [newColor, setnewColor] = useState({ name: "", code: { hex: "" })
+  const [newcolor, setnewColor] = useState({ color: "", code:{hex: "" }})
   const editColor = color => {
     setEditing(true);
     setColorToEdit(color);
   };
 
-  const saveEdit = e => {
+  const saveEdit = async e => {
     e.preventDefault();
-    axiosWithAuth().put(`/colors/${colorToEdit.id}`, colorToEdit)
-      .then(res => colors.map(color => color.id === res.data.id ? updateColors([...colors, { [color]: res }]) : ""), console.log(colors))
-      .catch(err => console.error(err))
+    await( axiosWithAuth().put(`/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res =>  console.log('colors',colors,"res",res,"res.data.code.hex",res.data.code.hex))
+      .catch(err => console.error(err)))
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth().get(`/colors`).then(res=>updateColors(res.data)).then(_=>console.log(colors)).catch(err=>console.error(err))
   };
 
-  const deleteColor = color => {
+  const deleteColor = async color => {
     // make a delete request to delete this color
-    axiosWithAuth().delete(`colors/${color.id}`).then(res => console.log(res)).catch(err => console.error(err))
+    await(axiosWithAuth().delete(`colors/${color.id}`).then(res => console.log(res)).catch(err => console.error(err)))
+    axiosWithAuth().get(`/colors`).then(res=>updateColors(res.data)).then(_=>console.log(colors)).catch(err=>console.error(err))
   };
-  const addNewColor = (e) => {
-    e.preventDefault()
-    axiosWithAuth().post(`/colors`, newColor)
+  const addNewColor =async(event,newcolor) => {
+    event.preventDefault()
+    console.log('newcolor:',newcolor)
+    await (axiosWithAuth().post(`/colors`, newcolor)
       .then(res => console.log(res.data))
-      .catch(err => console.error(err))
-  }
+      .catch(err => console.error(err)))
+      axiosWithAuth().get(`/colors`).then(res=>updateColors(res.data)).then(_=>console.log(colors)).catch(err=>console.error(err))
+    };
+
+  
+ 
   return (
     <div className="colors-wrap">
       <p>colors</p>
@@ -92,18 +99,21 @@ const ColorList = ({ colors, updateColors }) => {
       )}
       <div className="spacer" />
       {/* stretch - build another form here to add a color */}
-      <form onSubmit={addNewColor}>
+      <form onSubmit={event=>addNewColor(event,newcolor)}>
         <input
-          placeholder="colorname"
-          name="colorName"
-          value={newColor.name}
-          onChange={setnewColor(...newColor, newColor.name)
-          } />
+          placeholder="newcolor"
+          type="text"
+          name="color"
+          value={newcolor.color}
+          onChange={e=>setnewColor({...newcolor, color:e.target.value})}
+           
+           />
         <input
           placeholder="colorCodeHex"
+          type="text"
           name="hex"
-          value={newColor.code.hex}
-          onChange={setnewColor(...newColor, newColor.code.hex)}
+          value={newcolor.code.hex}
+          onChange={e=>setnewColor({...newcolor, code:{hex:e.target.value}})}
         />
         <button type="submit">add new color</button>
       </form>
