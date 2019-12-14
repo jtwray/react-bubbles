996cb0c80757a6b758daf89a4f5718638fc6c8ff
tucker <tuckerwray84@gmail.com>
2019-12-14 00:01:30 -0500

    deleted erroneous code in utils directory

--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -6,22 +6,11 @@ export const axiosWithAuth = () => {
     return axios.create({
         baseURL: "http://localhost:5000/api",
         headers: {
+          'Authorization': token,
             'Content-Type': 'application/json',
-            'Authorization': `${token}`,
         },
     })
 }
 
 
 
-{/**
-    
-    const login = () => {
-  axios.post('endpoint/here', userCredentials)
-    .then(res => {
-      localStorage.setItem('token', res.data.token);
-      props.history.push('/dashboard');
-    }
-}
-
-*/}
